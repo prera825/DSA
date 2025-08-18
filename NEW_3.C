13c929ce09cd3b24454618371247d7f29fd35658
prera825 <reddyprerana19@gmail.com>
2025-08-18 19:07:50 +0530

    Add files via upload

---
+++ NEW_3.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a= 15;
+	int b= 5;
+	int mutiplication = a*b;
+	printf("%d\n",a*b);
+	printf("a*b of %d\n",a*b);
+	
+	int i; {
+	for ( i=1;i<=20;i++)
+	printf("%d*%d = %d\n",a*b,i,a*b*i);
+	}
+	return 0;
+}
